c77f4415704a15555121ed1cb773b2e0978b64e5
Med12-q <smashsouare82@gmail.com>
2026-06-08 09:13:40 +0000

    fix: dynamic import FloatingChat with ssr:false to prevent prerender crash

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,17 +1,23 @@
 import type { ReactNode } from 'react'
-import { Navbar } from '@/components/navbar'
-import { Footer } from '@/components/footer'
-import { CyberBackground } from '@/components/background-fx'
-import { FloatingChat } from '@/components/floating-chat'
+  import dynamic from 'next/dynamic'
+  import { Navbar } from '@/components/navbar'
+  import { Footer } from '@/components/footer'
+  import { CyberBackground } from '@/components/background-fx'
 
-export default function SiteLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative flex min-h-screen flex-col">
-      <CyberBackground />
-      <Navbar />
-      <main className="relative z-10 flex-1">{children}</main>
-      <Footer />
-      <FloatingChat />
-    </div>
+  const FloatingChat = dynamic(
+    () => import('@/components/floating-chat').then(m => m.FloatingChat),
+    { ssr: false }
   )
-}
+
+  export default function SiteLayout({ children }: { children: ReactNode }) {
+    return (
+      <div className="relative flex min-h-screen flex-col">
+        <CyberBackground />
+        <Navbar />
+        <main className="relative z-10 flex-1">{children}</main>
+        <Footer />
+        <FloatingChat />
+      </div>
+    )
+  }
+  
